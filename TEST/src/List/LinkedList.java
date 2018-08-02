9b1c15a89af9ab0475dd5db5ada23ab011530b77
gnsgmlckdgk <gnsgmlckdgk@naver.com>
2018-08-02 19:17:37 +0900

    주석 위치 수정

--- TEST/src/List/LinkedList.java
+++ TEST/src/List/LinkedList.java
@@ -28,12 +28,6 @@ public class LinkedList<T> implements List<T>{
 	
 	
 	/* s:ADT */
-	
-	/**
-	 * 연결리스트에 data 삽입
-	 * @param plist
-	 * @param data
-	 */
 	public void insert(T data) {
 		Node<T> newNode = new Node<T>();
 		newNode.data = data;
@@ -43,10 +37,6 @@ public class LinkedList<T> implements List<T>{
 		this.numOfData++;
 	}
 	
-	/**
-	 * 연결리스트 첫번째 data 조회
-	 * @return 데이터 없으면 0, 있으면 1
-	 */
 	public boolean first() {
 		
 		// 데이터 없음
@@ -60,10 +50,6 @@ public class LinkedList<T> implements List<T>{
 		return true;
 	}
 	
-	/**
-	 * 연결리스트 첫번째 이후 data 조회
-	 * @return 데이터 없으면 0, 있으면 1
-	 */
 	public boolean next() {
 		
 		// 데이터 없음
@@ -76,11 +62,7 @@ public class LinkedList<T> implements List<T>{
 		
 		return true;
 	}
-	
-	/**
-	 * 마지막 반환 data 삭제
-	 * @return 삭제된 데이터
-	 */
+
 	public T remove() {
 	
 		T delData = this.cur.data;
@@ -93,27 +75,16 @@ public class LinkedList<T> implements List<T>{
 		
 		return delData;
 	}
-	
-	/**
-	 * 연결리스트에 저장되어있는 데이터 수 반환
-	 * @return 데이터 수
-	 */
+
 	public int count() {
 		
 		return this.numOfData;
 	}
-	
-	/**
-	 * 선택 위치의 데이터 반환
-	 * @return 현재 위치 데이터
-	 */
+
 	public T getData() {
 		return this.cur.data;
 	}
-	
-	/**
-	 * 모든 데이터 출력
-	 */
+
 	public void showAllData() {
 		
 		if(first()) {
--- TEST/src/List/List.java
+++ TEST/src/List/List.java
@@ -2,12 +2,48 @@ package List;
 
 public interface List<T> {
 	/* s:LinkedList ADT */
+	
+	/**
+	 * 연결리스트에 data 삽입
+	 * @param plist
+	 * @param data
+	 */
 	public abstract void insert(T data);
+	
+	/**
+	 * 연결리스트 첫번째 data 조회
+	 * @return 데이터 없으면 0, 있으면 1
+	 */
 	public abstract boolean first();
+	
+	/**
+	 * 연결리스트 첫번째 이후 data 조회
+	 * @return 데이터 없으면 0, 있으면 1
+	 */
 	public abstract boolean next();
+	
+	/**
+	 * 마지막 반환 data 삭제
+	 * @return 삭제된 데이터
+	 */
 	public abstract T remove();
+	
+	/**
+	 * 연결리스트에 저장되어있는 데이터 수 반환
+	 * @return 데이터 수
+	 */
 	public abstract int count();
+	
+	/**
+	 * 선택 위치의 데이터 반환
+	 * @return 현재 위치 데이터
+	 */
 	public abstract T getData();
+	
+	/**
+	 * 모든 데이터 출력
+	 */
 	public abstract void showAllData();
+	
 	/* e:LinkedList ADT */
 }
